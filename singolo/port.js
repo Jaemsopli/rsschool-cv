2564bf06633c1bda64598ed423431b5fb1e47b31
Jaemsopli <Babby07@mail.ru>
2020-12-12 16:24:07 +0300

    feat: The slider toggle function is working. Removed the extra switch button.

--- singolo/port.js
+++ singolo/port.js
@@ -1,10 +1,51 @@
 
 const turnOffTheScreen = document.querySelector('.button-circle');
-const screen = document.querySelector('.phone-picture');
+
 const next = document.querySelector('.next');
 const prev = document.querySelector('.prev');
-const blue = document.querySelector('.background-container-2');
-const roz = document.querySelector('.background-container');
+const slide = document.getElementsByClassName('slides');
+slideIndex = 1;
+
+
+showSlides(slideIndex);
+
+function showSlides (n) {
+    if (n < 1) {
+        slideIndex = slide.length;
+    } else if (n > slide.length) {
+        slideIndex = 1;
+    }
+    for (let i = 0; i < slide.length; i++) {
+        slide[i].style.display = 'none';
+    }
+    slide[slideIndex - 1].style.display = 'block';
+}
+
+function plusSlides (n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide (n) {
+    showSlides(slideIndex = n)
+}
+
+prev.onclick = function () {
+    plusSlides(-1);
+}
+next.onclick = function () {
+    plusSlides(1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 document.getElementById("sidebar").classList.toggle('exit'); 
@@ -14,61 +55,7 @@ let scr = document.getElementsByClassName('phone-picture');
 for(let i = 0; i < btn.length; i++)
 for(let i = 0; i < scr.length; i++)
 btn[i].onclick = function()  {scr[i].classList.toggle('turn');}
-    
-//next.onclick = function()  {blue.classList.remove('disable');}  работает неккоретно
-//next.onclick = function()  {roz.classList.toggle('disable');}
 
-
-
-// turnOffTheScreen.onclick = function() {screen.classList.toggle('turn');}
-
-/*i = 0;
-fon.onclick = function() {
-    this.style.background = 'blue';
-this.style.background = 'red';
-  i++;
-}
-
-let counter = 1;
-const size = slideImg[0].clientWidth;
-slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-
-nextBtn.addEventListener('click', ()=> {
-if (counter >= slideImg.length - 1) return;
-slide.style.transition = "transform 0.4s ease-in-out";
-counter++;
-slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-});
-
-prevBtn.addEventListener('click', ()=> {
-if (counter <= 0) return;
-slide.style.transition = "transform 0.4s ease-in-out";
-counter--;
-slide.style.transform = 'translateX(' + ( -size * counter ) + 'px)';
-});
-
-slide.addEventListener('transitionend', ()=> {
-if (slideImg[counter].id === 'last') {
-slide.style.transition = "none";
-counter = slideImg.length -2;
-slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-}
-if (slideImg[counter].id === 'first') {
-slide.style.transition = "none";
-counter = slideImg.length - counter;
-slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-}
-});
-
-
-
-/*{
- {
-    console.log(slideImg[i]);
-    
-}
-}
-*/
 
 
 
